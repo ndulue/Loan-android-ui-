a19e7d7ea09e4295a28cc912b57df815428d8d22
ndulue <mail2emekandulue@gmail.com>
2020-06-27 23:19:02 +0100

    first commit

---
+++ app/src/main/java/com/example/loanui/LoanCalculatorActivity.java
@@ -0,0 +1,14 @@
+package com.example.loanui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoanCalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loan_calculator);
+    }
+}
