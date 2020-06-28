77aa28cb464d72a5f35a6372b1de1c05e59adf06
ndulue <mail2emekandulue@gmail.com>
2020-06-29 00:14:33 +0100

    Final Update

--- app/src/main/java/com/example/loanui/MainActivity.java
+++ app/src/main/java/com/example/loanui/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.loanui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button applyLoan;
+    Button loanCalculator;
+    Button loanApproved;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        applyLoan = findViewById(R.id.applyLoan);
+        applyLoan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ApplyLoanActivity.class));
+            }
+        });
+
+
+        loanApproved = findViewById(R.id.loanApproved);
+        loanApproved.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoanAprovedActivity.class));
+            }
+        });
+
+        loanCalculator = findViewById(R.id.loanCalculator);
+        loanCalculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoanCalculatorActivity.class));
+            }
+        });
     }
 }
